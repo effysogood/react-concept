1e56037fe9962bd5f7862756d83a83043e33935a
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-06-28 16:16:49 -0400

    feat: Create new badge icon for Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-export default function Profile({ image, name, position }) {
+// export default function Profile({ image, name, position, new: isNew = 0 }) {
+//   return (
+//     <div className='profile'>
+//       <div className={`photo ${isNew === 1 ? 'new' : ''}`}>
+//         <img src={image} alt='profile' />
+//       </div>
+//       <h1>{name}</h1>
+//       <p>{position}</p>
+//     </div>
+//   );
+// }
+
+export default function Profile({ image, name, position, isNew = false }) {
   return (
     <div className='profile'>
-      <img className='photo' src={image} alt='profile' />
+      <div className='profileImg'>
+        <img src={image} alt='profile' />
+        {isNew && <span className='badge'>New</span>}
+      </div>
       <h1>{name}</h1>
       <p>{position}</p>
     </div>
