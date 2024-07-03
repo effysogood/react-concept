01a151bed1eab36ee17f409d82f802008cfa8860
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-07-03 17:48:39 -0400

    comment: useEffect 의존성

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,6 +20,12 @@ export default function Products() {
     };
   }, [checked]); // 의존성의 데이터가 변경될때마다 재렌더링
 
+  /* useEffect 없이 데이터를 불러오는 로직을 외부에 작성 시 생기는 이슈
+  상태가 변경(setProducts)되면 리액트는 Product 함수를 재호출 ->
+  그럼 결국 fetch <-> setProducts가 무한 반복되며 메모리 성능 저하될 것!
+  처음 컴포넌트가 생성되었을 때 네트워크 통신 한번만 요청되도록 useEffect
+  */
+
   return (
     <>
       <input type='checkbox' id='checkbox' onClick={handleChecked} />
