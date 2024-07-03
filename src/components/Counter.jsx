a4320d6e0d922976f4f23945aca3019d065ae852
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-07-03 16:36:44 -0400

    fix: 패키지 빌드 오류 수정

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 
 export default function Counter({ total, onClick }) {
   const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
-    onClick();
-  };
   return (
     <div className='counter'>
-      <p>
-        <span className='number'>{count}</span>
-        <small>/ {total}</small>
+      <p className='number'>
+        {count} <span className='total'>/{total}</span>
       </p>
-      <button className='button' onClick={handleClick}>
-        ADD +
+      <button
+        className='button'
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          onClick();
+        }}
+      >
+        Add +
       </button>
     </div>
   );
