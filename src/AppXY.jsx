b3b671199fc332c355d4d7032b810e7e7f664c62
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-07-04 17:29:41 -0400

    feat: 상태를 객체 단위로 보관 및 업데이트하기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -5,7 +5,13 @@ export default function AppXY() {
   const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
+    // 📍 동일한 성격의 데이터, 객체로 관리
     setCoordinates({ x: e.clientX, y: e.clientY });
+    // 📍 만약 수평으로만 이동하도록 제어한다면? 이전 값 저장
+    // setCoordinates((prev) => ({ x: e.clientX, y: prev.y }));
+    // 📍 만약 z좌표를 추가할 경우, 다중 데이터 관리 시
+    // setCoordinates((prev) => ({...prev, x: e.clientX}))
+
     console.log(`x: ${coordinates.x}, y: ${coordinates.y}`);
   };
 
