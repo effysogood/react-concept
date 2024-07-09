b90705c87542adca733cd3a7cce83fa1e3b1a6e4
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-07-08 23:55:53 -0400

    feat: immer로 상태 관리

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppMentorsReduce from './AppMentorsReduce';
+import AppMentorsImmer from './AppMentorsImmer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentorsReduce />
+    <AppMentorsImmer />
   </React.StrictMode>
 );
 
