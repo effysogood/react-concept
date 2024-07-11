6c0c4a752850c9febe05e79448dd195fadb8c476
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-07-10 23:51:07 -0400

    feat: form 생성 및 상태관리

---
+++ src/AppForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+export default function AppForm() {
+  const [form, setForm] = useState({ name: '', email: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm(...form, { [name]: value });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name : </label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          value={form.name}
+          onChange={handleChange}
+        />
+        <label htmlFor='name'>Email : </label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          value={form.email}
+          onChange={handleChange}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  );
+}
