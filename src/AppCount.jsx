4738b134095acd77fc03be7d66254feb35b87c58
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-07-30 08:19:26 +0900

    feat: appcount 컴포넌트 추가

---
+++ src/AppCount.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from 'react';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중..');
+  return users.filter((user) => user.active).length;
+}
+
+export default function AppCount() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [user, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true,
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false,
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({ username: '', email: '' });
+  };
+  return <></>;
+}
