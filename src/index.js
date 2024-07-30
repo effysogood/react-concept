2e9a760ac5f68296e6fdc4e44a37fb409a92fe4c
Saeyoung Choi <effysogood@gmail.com>
2024-07-30 14:51:55 +0900

    style: postCSS 스타일링

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppProducts from './basic/AppProducts';
+import PostCss from './styling/postCss/Buttons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <PostCss />
   </React.StrictMode>
 );
 
---
+++ src/styling/postCss/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button1 from './Button1';
+import Button2 from './Button2';
+
+export default function Buttons() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+}
+
+// 클래스명이 해시화되어 중복 오류를 줄여줌
+// 기존 css 스타일링 방식과 동일하게 작성!
